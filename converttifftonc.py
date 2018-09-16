d29dabfe0ba0ca0fa3382223f2f104bc57193e95
rocknrolltt <ting.wu@hotmail.com>
2018-09-16 16:56:16 -0400

    Add files via upload

--- converttifftonc.py
+++ converttifftonc.py
@@ -8,58 +8,68 @@ import rasterio
 
 
 with rasterio.drivers():
-	src=rasterio.open(r"V:\twu\fireline\travelers\access.tiff","r")
-	sr1=rasterio.open(r"D:\twu\fireline\travelers\.tiff","r")
+	src=rasterio.open(r"D:\twu\fireline\travelers\AF.tiff","r")
+	sr1=rasterio.open(r"D:\twu\fireline\travelers\access.tiff","r")
 	sr2=rasterio.open(r"D:\twu\fireline\travelers\slope.tiff","r")
 	dst_transform=src.transform
 	dst_width=src.width
-	print (dst_width)
+	#print (dst_width)
 	
 	dst_height=src.height
-	print (dst_height)
-	print (dst_transform)
+	#print (dst_height)
+	#print (dst_transform)
 	#xmin = dst_transform[0]
 	xmin=-124.8633388437681333
 	#xmax = dst_transform[0] + dst_transform[1]*dst_width
 	xmax=-93.4969608019406024
-	print (xmax)
+	#print (xmax)
 	#ymin = dst_transform[3] + dst_transform[5]*dst_height
 	ymin=25.8314103514306339
-	print(ymin)
+	#print(ymin)
 	#ymax = dst_transform[3]
 	ymax=49.0579894904837488
+	print (dst_transform)
+	print (dst_transform[1])
+	print (dst_transform[5])
 
 	dst_width=dst_width
 	dst_height=dst_height
-	outf=Dataset(r'V:\twu\fireline\travelers\access.nc','w',format='NETCDF4')
-	lats=np.linspace(ymin,ymax,dst_width)
-	print (len(lats))
+	outf=Dataset(r'D:\twu\fireline\travelers\WFHS.nc','w',format='NETCDF4')
+	lats=np.linspace(ymin,ymax,dst_height)
+	#print (len(lats))
 	
-	lons=np.linspace(xmin,xmax,dst_height)
-	print (len(lons))
+	lons=np.linspace(xmin,xmax,dst_width)
+	#print (len(lons))
+	if dst_transform[1] < 0:
+		lons = lons[::-1]
+	if dst_transform[5] < 0:
+		lats = lats[::-1]
 
 	lat=outf.createDimension('lon',len(lons))
 	lon=outf.createDimension('lat',len(lats))
 	longitudes=outf.createVariable('longitude',np.float64,('lon',))
 	latitudes=outf.createVariable('latitude',np.float64,('lat',))
-	SHIA=outf.createVariable('ACCESS',np.uint8,('lon','lat'))
-	print (longitudes.shape)
-	print (lons.shape)
+	SHIA=outf.createVariable('AREASCORE',np.uint8,('lat','lon'))
+	#print (longitudes.shape)
+	#print (lons.shape)
+	#lons=lons[::-1]
 	longitudes[:]=lons
+	#lats=lats[::-1]
 	latitudes[:]=lats
 	im=src.read()
-	im=src.fliplr(im)
 	src.close()
-	# im1=sr1.read()
-	# sr1.close()
+	im1=sr1.read()
+	sr1.close()
 	
-	# im2=sr2.read()
-	# sr2.close()
-	# im=im*im2+im1
-	# im[im>=250]==255
+	im2=sr2.read()
+	sr2.close()
+	valid=im!=255
+	im[valid] *= im2[valid]
+	im[valid] += im1[valid]
+	im[im>=250]==255
 	SHIA[:,:]=im
 
-	outf.description="Access for 13 states"
+	outf.description="AREASCORE for 13 states"
 	longitudes.units="degrees east"
 	latitudes.units='degrees north'
 
--- mergefinalnc.py
+++ mergefinalnc.py
@@ -4,23 +4,23 @@ import glob
 
 import numpy as np
 
-# d=xr.open_dataset(r'D:\twu\fireline\travelers\AF.nc')
-# d=d.set_index(lat='latitude',inplace=True)
-# d=d.set_index(lon='longitude',inplace=True)
-# d.to_netcdf(r'D:\twu\fireline\travelers\new_AF.nc')
-#a=xr.open_dataset(r'V:\twu\fireline\travelers\access.nc')
+#d=xr.open_dataset(r'D:\twu\fireline\travelers\AF.nc')
+#d=d.set_index(lat='latitude',inplace=True)
+#d=d.set_index(lon='longitude',inplace=True)
+#d.to_netcdf(r'D:\twu\fireline\travelers\new_AF.nc')
+#a=xr.open_dataset(r'D:\twu\fireline\travelers\access.nc')
 #a=a.set_index(lat='latitude',inplace=True)
 #a=a.set_index(lon='longitude',inplace=True)
-#a.to_netcdf(r'V:\twu\fireline\travelers\new_access.nc')
-# b=xr.open_dataset(r'D:\twu\fireline\travelers\slope.nc')
-# b=b.set_index(lat='latitude',inplace=True)
-# b=b.set_index(lon='longitude',inplace=True)
-# b.to_netcdf(r'D:\twu\fireline\travelers\new_slope.nc')
-# c=xr.open_dataset(r'D:\twu\fireline\travelers\fuel.nc')
-# c=c.set_index(lat='latitude',inplace=True)
-# c=c.set_index(lon='longitude',inplace=True)
-# c.to_netcdf(r'D:\twu\fireline\travelers\new_fuel.nc')
-#f=xr.open_dataset(r'D:\twu\fireline\travelers\SHIA.nc')
+#a.to_netcdf(r'D:\twu\fireline\travelers\new_access.nc')
+#b=xr.open_dataset(r'D:\twu\fireline\travelers\slope.nc')
+#b=b.set_index(lat='latitude',inplace=True)
+#b=b.set_index(lon='longitude',inplace=True)
+#b.to_netcdf(r'D:\twu\fireline\travelers\new_slope.nc')
+#c=xr.open_dataset(r'D:\twu\fireline\travelers\fuel.nc')
+#c=c.set_index(lat='latitude',inplace=True)
+#c=c.set_index(lon='longitude',inplace=True)
+#c.to_netcdf(r'D:\twu\fireline\travelers\new_fuel.nc')
+#f=xr.open_dataset(r'D:\twu\fireline\travelers\shia.nc')
 #f=f.set_index(lat='latitude',inplace=True)
 #f=f.set_index(lon='longitude',inplace=True)
 #f.to_netcdf(r'D:\twu\fireline\travelers\new_shia.nc')
@@ -31,22 +31,37 @@ import numpy as np
 
 # mgd=xr.merge([d,a,b,c,f,e],join='outer')
 
-hh=xr.open_dataset(r'V:\twu\fireline\travelers\final\fuel_shia_final.nc')
+# hh=xr.open_dataset(r'V:\twu\fireline\travelers\final\fuel_shia_final.nc')
 
 
-dd=xr.open_dataset(r'V:\twu\fireline\travelers\final\third_access_AF_final.nc')
-cc=xr.open_dataset(r'V:\twu\fireline\travelers\final\WFHS_Slope_final.nc')
+# dd=xr.open_dataset(r'V:\twu\fireline\travelers\final\third_access_AF_final.nc')
+# cc=xr.open_dataset(r'V:\twu\fireline\travelers\final\WFHS_Slope_final.nc')
 
-ds = xr.merge([hh, dd,cc], join='outer')
+# ds = xr.merge([hh, dd,cc], join='outer')
 
-# for fname in glob.glob(r'V:\twu\fireline\travelers\final\*.nc'):
-	# ds=xr.open_mfdataset(fname)
-ds.to_netcdf(r"V:\twu\fireline\travelers\fireline_all.nc")
-
-
+# # for fname in glob.glob(r'V:\twu\fireline\travelers\final\*.nc'):
+	# # ds=xr.open_mfdataset(fname)
+# ds.to_netcdf(r"V:\twu\fireline\travelers\fireline_all.nc")
 
 
-#datasets = xr.merge([hh,ff,ss,ac,fuel,shia], join='outer')
+hh=xr.open_dataset(r'D:\twu\fireline\travelers\new_AF.nc')
+hh=hh.fillna(255).astype(np.uint8)
+
+dd=xr.open_dataset(r'D:\twu\fireline\travelers\new_access.nc')
+dd=dd.fillna(255).astype(np.uint8)
 
 
-#hh.to_netcdf(r"D:\twu\fireline\travelers\Fireline_13states.nc")
+ff=xr.open_dataset(r'D:\twu\fireline\travelers\new_slope.nc')
+ff=ff.fillna(255).astype(np.uint8)
+ss=xr.open_dataset(r'D:\twu\fireline\travelers\new_shia.nc')
+ss=ss.fillna(255).astype(np.uint8)
+aa=xr.open_dataset(r'D:\twu\fireline\travelers\new_fuel.nc')
+aa=aa.fillna(255).astype(np.uint8)
+tt=xr.open_dataset(r'D:\twu\fireline\travelers\new_WFHS.nc')
+tt=tt.fillna(255).astype(np.uint8)
+# # datasets = xr.merge([hh, dd], join='exact')
+
+datasets = xr.merge([hh,ff,ss,dd,aa,tt], join='outer')
+
+
+datasets.to_netcdf(r"D:\twu\fireline\travelers\Fireline_13states.nc")
